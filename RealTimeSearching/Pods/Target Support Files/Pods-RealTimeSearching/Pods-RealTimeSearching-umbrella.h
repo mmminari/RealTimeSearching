aff429e34d6d5fa51e5a9bd18a2ff0d64c7c5fb5
mmminari <mmminari@smtown.com>
2017-02-27 18:24:19 +0900

    install pod

---
+++ RealTimeSearching/Pods/Target Support Files/Pods-RealTimeSearching/Pods-RealTimeSearching-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_RealTimeSearchingVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RealTimeSearchingVersionString[];
+
---
+++ RealTimeSearching/Pods/Target Support Files/Pods-RealTimeSearchingTests/Pods-RealTimeSearchingTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RealTimeSearchingTests : NSObject
+@end
+@implementation PodsDummy_Pods_RealTimeSearchingTests
+@end
